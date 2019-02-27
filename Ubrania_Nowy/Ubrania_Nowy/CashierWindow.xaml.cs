4132e7d4a7c2faec03570513a56ab44e2f244fcf
Lukasz <lukasz.jablonski1991@gmail.com>
2019-02-27 19:03:22 +0100

    Dodane Okno cashier window

--- Ubrania_Nowy/Ubrania_Nowy/CashierWindow.xaml.cs
+++ Ubrania_Nowy/Ubrania_Nowy/CashierWindow.xaml.cs
@@ -19,9 +19,35 @@ namespace Ubrania_Nowy
     /// </summary>
     public partial class CashierWindow : Window
     {
+        int temp2 = 0;
+        private ERPDbContext _context = new ERPDbContext();
         public CashierWindow()
         {
             InitializeComponent();
+            Scaner_txt.Focus();
+          //  var cloth = _context.Clothes.ToList().Where(i => i.Agreement_Id == passId);
+
+
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            
+
+            if (e.Key == Key.Return)
+            {
+                int temp = Convert.ToInt32(Scaner_txt.Text);
+                Cloth cloth = _context.Clothes.Where(i => i.Id == temp).FirstOrDefault();
+
+                if (cloth.Sold == false)
+                { cloth.Sold = true; }
+                temp2 = temp2 + cloth.Price;
+                price_txt.Text = temp2.ToString();
+
+                _context.SaveChanges();
+
+                // MessageBox.Show("dziala");
+            }
         }
     }
 }
