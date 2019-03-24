fd7c924dad5631ecaa5a89e0702a6129f72fa668
Lukasz <lukasz.jablonski1991@gmail.com>
2019-03-24 21:55:05 +0100

    Added ASP.NET project

---
+++ Ubrania_Nowy/Ubrania_ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ubrania_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Ubrania_Nowy/Ubrania_ASP.NET2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ubrania_ASP.NET2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
